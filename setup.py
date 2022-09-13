a8a046e1ce0c245f272cd89a677280a431b9cf31
Sasen Perera <sasen.learnings@gmail.com>
2022-09-13 18:23:12 +0530

    3.0.1-beta: fixed bug in requirements and setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ long_description = open('README.md', "r", encoding="utf-8").read()
 
 setup(
     name="Lemon-Library",
-    version="3.0.0-Beta",
+    version="3.0.1-Beta",
     packages=find_packages(),
-    install_requires=["parse==1.19.0", "waitress==2.1.2", "WebOb==1.8.7"],
+    install_requires=["parse==1.19.0", "waitress==2.1.2", "WebOb==1.8.7", "whitenoise==6.2.0"],
     author="Sasen Perera",
     author_email="sas8.communications@gmail.com",
     description="An experimental Full-Stack Framework for Python",
