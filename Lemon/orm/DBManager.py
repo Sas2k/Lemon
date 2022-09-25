707fd26d13c9e885cd93cca05576281531bdecce
Sasen Perera <sasen.learnings@gmail.com>
2022-09-25 17:53:39 +0530

    Version 3.6.0: Added more ORM commands

--- Lemon/orm/DBManager.py
+++ Lemon/orm/DBManager.py
@@ -49,8 +49,69 @@ class SQLConnectionManager:
         except sql3.Error as er:
             print(f"SQLite error: {' '.join(er.args)}")
 
+    @commit
+    def delete(self, tablename, fields):
+        delete_command = f"DELETE FROM {tablename} WHERE {fields[0]} = {fields[1]}"
+        try:
+            self.cursor.execute(delete_command)
+        except sql3.Error as er:
+            print(f"SQLite error: {' '.join(er.args)}")
+
+    @commit
+    def update(self, tablename, fields):
+        update_command = f"UPDATE {tablename} SET {fields[0]} = {fields[1]} WHERE {fields[2]} = {fields[3]}"
+        try:
+            self.cursor.execute(update_command)
+        except sql3.Error as er:
+            print(f"SQLite error: {' '.join(er.args)}")
+
+    @commit
+    def select(self, tablename, fields):
+        select_command = f"SELECT {fields[0]} FROM {tablename} WHERE {fields[1]} = {fields[2]}"
+        try:
+            self.cursor.execute(select_command)
+            return self.cursor.fetchall()
+        except sql3.Error as er:
+            print(f"SQLite error: {' '.join(er.args)}")
+
+    @commit
+    def drop_table(self, tablename, fields):
+        drop_command = f"DROP TABLE IF EXISTS {tablename}"
+        try:
+            self.cursor.execute(drop_command)
+        except sql3.Error as er:
+            print(f"SQLite error: {' '.join(er.args)}")
+
+    @commit
+    def get_column(self, tablename, fields):
+        get_column_command = f"SELECT {fields[0]} FROM {tablename}"
+        try:
+            self.cursor.execute(get_column_command)
+            return self.cursor.fetchall()
+        except sql3.Error as er:
+            print(f"SQLite error: {' '.join(er.args)}")
+
+    @commit
+    def get_table(self, tablename, fields):
+        get_table_command = f"SELECT * FROM {tablename}"
+        try:
+            self.cursor.execute(get_table_command)
+            return self.cursor.fetchall()
+        except sql3.Error as er:
+            print(f"SQLite error: {' '.join(er.args)}")
+
     @property
     def show_tables(self):
+        command = "SELECT * FROM sqlite_master WHERE type='table';"
+        return self.cursor.execute(command)
+
+    @property
+    def show_columns(self):
+        command = "SELECT * FROM sqlite_master WHERE type='table';"
+        return self.cursor.execute(command)
+
+    @property
+    def show_rows(self):
         command = "SELECT * FROM sqlite_master WHERE type='table';"
         return self.cursor.execute(command)
 
