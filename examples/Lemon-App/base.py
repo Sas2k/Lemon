11759d826b60090fbc429d3bc1adb898c0e4d79e
Sasen Perera <sasen.learnings@gmail.com>
2022-10-05 16:32:39 +0530

    re-added base.py to Examples/Lemon-App/

---
+++ examples/Lemon-App/base.py
@@ -0,0 +1,14 @@
+from Lemon.orm import DBManager, migrations
+
+baseModel = DBManager.baseModel
+ClassBase = DBManager.base
+migrate = migrations.MigrateCommand
+
+class model(baseModel):
+    base_model = ClassBase
+    tablename = "model"
+    fields = ("field1", "field2")
+
+model_list = [model]
+
+migrate(model_list).migrate()
